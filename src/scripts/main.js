51d43bea7834e9f0ef84148111f57cccfba90a2c
The Old Jack <daymonblackgamer@gmail.com>
2024-05-22 15:24:07 -0300

    Estrutura completa

---
+++ src/scripts/main.js
@@ -0,0 +1,27 @@
+//Comportamento do nav
+
+/*const navbar = document.querySelector('.nav');
+
+document.addEventListener("scroll", function() {
+    const posicaoy = window.pageYOffset;
+    navbar.style.opacity = "0";
+    if (posicaoy == 0) {
+        navbar.style.opacity = "0";
+    } else {
+        navbar.style.opacity = "1";
+    }
+});
+
+window.addEventListener('scroll', function() {
+    var scrollTop = window.scrollY;
+    var fundo = document.querySelector('.hero');
+    var opacidade = scrollTop / 200; // Ajuste conforme necessário
+    var corParametro = 'rgba(26, 29, 41, 0.7)'; // Defina a cor do parâmetro aqui (no formato hexadecimal)
+    if (scrollTop < 500) { // Defina a posição para a mudança de cor
+        fundo.style.backgroundColor = `rgba(26, 29, 41, ${opacidade})`;
+        fundo.style.opacity = '0.7'
+    } else {
+      fundo.style.backgroundColor = corParametro; // Altera para a cor do parâmetro
+    }
+});
+*/
